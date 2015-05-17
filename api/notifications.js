f176ea6db532a19682c074196978107401d90121
kmcgaire <kevinmcgaire@gmail.com>
2015-05-16 20:00:20 -0400

    10am

--- api/notifications.js
+++ api/notifications.js
@@ -15,7 +15,7 @@ module.exports = function(router,
 
 	var millisecondInDay = 60*60*24*1000;
 	var now = new Date();
-	var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 0, 0, 0) - now;
+	var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
 	if (millisTill10 < 0) {
 	     millisTill10 += millisecondInDay; // it's after 10am, try 10am tomorrow.
 	}
