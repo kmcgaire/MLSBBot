1981c03a1667e001580f8b2c59ffe3d569f9f8f8
kmcgaire <kevinmcgaire@gmail.com>
2015-05-19 11:57:01 -0400

    remove backdoor

--- api/subscriptions.js
+++ api/subscriptions.js
@@ -45,23 +45,23 @@ module.exports = function (router, db){
 		});
 	});
 
-	router.put('/subscriptions', function (req, res){
-		var data = req.body;
-		console.log(req);
-		if (!data || !data.team || !data.username){
-			respond(res, 404, "username and team required");
-			return;
-		}
-		db.addSubscription(data.team, data.username, function (data){
-			if (data.err){
-				console.log('could not put ')
-				respond(res, 500, "");
-				return;
-			} else {
-				respond(res, 200, format("Added username=%s team=%s",username, team));
-				return;
-			}
-		});
-	});
+	// router.put('/subscriptions', function (req, res){
+	// 	var data = req.body;
+	// 	console.log(req);
+	// 	if (!data || !data.team || !data.username){
+	// 		respond(res, 404, "username and team required");
+	// 		return;
+	// 	}
+	// 	db.addSubscription(data.team, data.username, function (data){
+	// 		if (data.err){
+	// 			console.log('could not put ')
+	// 			respond(res, 500, "");
+	// 			return;
+	// 		} else {
+	// 			respond(res, 200, format("Added username=%s team=%s",username, team));
+	// 			return;
+	// 		}
+	// 	});
+	// });
 
 }
